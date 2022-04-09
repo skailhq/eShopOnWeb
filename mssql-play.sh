b8aa2b9e13df3b7ff914132ad6650051da0a683d
Geovani Morais <geovani@enotas.com.br>
2022-04-09 06:54:32 -0300

    Start mcr.microsoft.com/azure-sql-edge:latest and apply ef updates.

---
+++ mssql-play.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+podman run -e SA_PASSWORD=@someThingComplicated1234 -e ACCEPT_EULA=Y --rm=true -p 1433:1433/tcp mcr.microsoft.com/azure-sql-edge:latest &
+
+
+sleep 30
+
+cd src/Web
+dotnet restore
+dotnet tool restore
+dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj --configuration Podman
+dotnet ef database update -c appidentitydbcontext  -p ../Infrastructure/Infrastructure.csproj -s Web.csproj --configuration Podman
