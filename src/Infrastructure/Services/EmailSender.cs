cc70d08abaac35a70897803b9f398e6925e824e7
Geovani Morais <geovani@enotas.com.br>
2022-04-11 12:41:54 -0300

    Console project SendMail.

--- src/Infrastructure/Services/EmailSender.cs
+++ src/Infrastructure/Services/EmailSender.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using System.Net.Sockets;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Microsoft.eShopWeb.Infrastructure.Services;
 
@@ -19,6 +23,34 @@ public class EmailSender : Microsoft.AspNetCore.Identity.UI.Services.IEmailSende
     {
         // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
         _logger.LogInformation(message);
+        
+        // Used on demonstrations (using command "nc -l 2525 -v" to show message)
+        var addr = Environment.GetEnvironmentVariable("SMTP_SERVER");
+        try
+        {
+            var _addr = addr?.Split(":");
+            var server = _addr.IsNullOrEmpty() ? "127.0.0.1" : _addr?[0] ?? "127.0.0.1";
+            var _port = _addr.IsNullOrEmpty() || _addr?.Length < 2 ? "127.0.0.1" : _addr?[1];
+
+            var result = Int32.TryParse(_port, out var port);
+            TcpClient client = new TcpClient(server, result ? port : 2525);
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);
+
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            _logger.LogInformation(e.Message);
+        }
+        
         return Task.CompletedTask;
     }
 }
---
+++ src/SendEmail/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.eShopWeb.Web.Configuration;
+using Microsoft.Extensions.Hosting;
+using Skail.Platform.Runtime;
+using Skail.Platform.Runtime.Core.DependencyInjection;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.UseServiceProviderFactory(PlatformServiceProviderFactory.Current);
+builder.ConfigureServices( (hostContext, services) =>
+{
+    services.AddCoreServices(hostContext.Configuration);
+});
+var funcHost = builder.Build();
+await Platform.Initialize(funcHost).RunAsync();
