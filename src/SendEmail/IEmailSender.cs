96adf3cac90253ced6ac5f01e1c8ba2200c08891
Rodrigo Reis <rodrigo@enotas.com.br>
2022-05-05 10:15:24 -0300

    update Skail.Runtime.Platform to alpha.27 and add [ServiceInterface] to ISendEmail

--- src/SendEmail/IEmailSender.cs
+++ src/SendEmail/IEmailSender.cs
@@ -1,7 +1,9 @@
-﻿using Skail.Platform.Runtime.Schedulers.Abstractions;
+﻿using Skail.Platform.Runtime.Configuration.Attributes;
+using Skail.Platform.Runtime.Schedulers.Abstractions;
 
 namespace Microsoft.eShopWeb.Infrastructure.Services;
 
+[ServiceInterface]
 public interface IEmailSender : IDelayable
 {
     Task SendEmailAsync(string email, string subject, string htmlMessage);
