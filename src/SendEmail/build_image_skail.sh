710c206c7e328d8f17b892e8e4aafac4b1b4b015
Geovani Morais <geovani@enotas.com.br>
2022-05-13 12:04:46 -0300

    Change namespace

--- src/SendEmail/build_image_skail.sh
+++ src/SendEmail/build_image_skail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TAG=v0.0.1
 
-podman build -t registry.digitalocean.com/skail/func/sendemail:$TAG --build-arg feed_name=$FEED_NAME --build-arg feed_user=$FEED_USER --build-arg feed_password=$FEED_PASSWORD  -f Dockerfile_skail .
-podman push registry.digitalocean.com/skail/func/sendemail:$TAG
+podman build -t registry.digitalocean.com/skail/eshoppingweb/sendemail:$TAG --build-arg feed_name=$FEED_NAME --build-arg feed_user=$FEED_USER --build-arg feed_password=$FEED_PASSWORD  -f Dockerfile_skail .
+podman push registry.digitalocean.com/skail/eshoppingweb/sendemail:$TAG
